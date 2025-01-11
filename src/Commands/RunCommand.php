1f1ee5211ea8b0ba5bfc66586e8ec246ea0cb1f9
Tony Karlsson <tony.karlsson@nbts.fi>
2025-01-11 17:50:00 +0200

    Do major additions+changes to RunCommand

--- src/Commands/RunCommand.php
+++ src/Commands/RunCommand.php
@@ -19,40 +19,74 @@ class RunCommand extends Command
         $part = $this->argument('part');
         $year = $this->option('year') ?: date('Y');
 
+        // Instantiate a class for the correct puzzle
         $namespace = Str::of(config('aochelper.solution.path'))
             ->replace('/', '\\')
             ->ucfirst();
-
         $className = sprintf('%s\\Year%s\\Day%sPart%s', $namespace, $year, $day, $part);
-
         if (! class_exists($className)) {
-            $this->components->error('Class not found');
-
+            $this->components->error("No task implementation found for year {$year} day {$day} part {$part}");
             return self::FAILURE;
         }
 
         /** @var Puzzle $solution */
         $solution = new $className;
 
+        // These are needed for console I/O
         $solution->setOutput($this->output);
         $solution->setInput($this->input);
 
+        // Print puzzle banner
         $puzzleName = $solution->getPuzzleName();
-        $verbosity = OutputInterface::VERBOSITY_NORMAL;
-        $length = Str::length(strip_tags($puzzleName)) + 12;
+        $partAsText = ($part == 1) ? 'One' : 'Two';
+        $puzzleBanner = "AoC {$year} - Day {$day}: {$puzzleName} - Part {$partAsText}";
+        $length = strlen($puzzleBanner) + 12;
 
-        $this->info(str_repeat('*', $length), $verbosity);
-        $this->info('*     '.$puzzleName.'     *', $verbosity);
-        $this->info(str_repeat('*', $length), $verbosity);
-        $this->info('', $verbosity);
+        $this->info(str_repeat('*', $length));
+        $this->info('*     '.$puzzleBanner.'     *');
+        $this->info(str_repeat('*', $length));
+        $this->newLine();
 
+        // Read puzzle input from file
+        /*
+        $puzzleInputFile = base_path("/input/{$year}/d{$day}.data");
+        $puzzleInput = trim(File::get($puzzleInputFile));
+        */
+        $puzzleInput = '';
+        $solution->setPuzzleInput($puzzleInput);
+
+        // Run the actual solution
         $error = $solution->solve();
 
         if ($error) {
             $this->warn('De fou feil me '.$solution->getPuzzleAnswer());
-        } else {
-            $this->info($solution->getPuzzleAnswer().' va rätt svar');
+            return $error;
         }
+
+        // Print the answer from the solution
+        $answerDescription = $solution->getPuzzleAnswerDescription();
+        $answer = $solution->getPuzzleAnswer();
+        render(<<<HTML
+            <div><i>{$answerDescription}:</i> <b><u>{$answer}</u></b></div>
+        HTML);
+
+        // Check the result against answer file, if one exists
+        /*
+        $answerFile = base_path("/answers/{$year}/d{$day}p{$part}.data");
+        if (File::exists($answerFile)) {
+            $storedAnswer = trim(File::get($answerFile));
+            if ($storedAnswer == $answer) {
+                $this->comment("Answer matches stored answer file.");
+            } else {
+                $this->error("Answer differs from stored answer file: {$storedAnswer}");
+            }
+        }
+        $this->newLine();
+        */
+
+        // Print the duration
+        $duration = now()->diff(Carbon::createFromTimestamp(LARAVEL_START))->forHumans(['minimumUnit' => 'ms', 'short' => true, 'parts' => 2]);
+        $this->comment("Duration: {$duration}");
 
         return self::SUCCESS;
     }
