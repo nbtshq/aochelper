a3114141fd26634c6215f5d5a8492eb08e431195
Tony Karlsson <tony.karlsson@nbts.fi>
2025-01-11 23:22:59 +0200

    Base AocdConfigTestCommand on Illuminate\Console\Command instead of LaravelZero Command class

--- src/Commands/AocdConfigTestCommand.php
+++ src/Commands/AocdConfigTestCommand.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace NorthernBytes\AocHelper\Commands;
 
+use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
-use LaravelZero\Framework\Commands\Command;
 
 class AocdConfigTestCommand extends Command
 {
